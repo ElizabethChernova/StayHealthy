1c4bba7e6212e14667662adbea0fb03dedb7c94e
Elizabeth_Chernova <elizabethchernova@gmail.com>
2022-06-20 13:20:55 +0300

    DateScedule

--- app/src/main/java/com/example/entities/DayScedule.java
+++ app/src/main/java/com/example/entities/DayScedule.java
@@ -1,4 +1,15 @@
 package com.example.entities;
 
+import java.util.Date;
+import java.util.HashMap;
+
 public class DayScedule {
+    /**
+     *dependance on time to something
+     */
+    HashMap<Date,String> schedule =new HashMap();
+    /**
+     *dependance on time to Pills
+     */
+    HashMap<Date,Pill> pillSchedule =new HashMap();
 }
