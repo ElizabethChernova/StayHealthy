cc5f5e74ecf2803c9c94b22d26963a61e3d65050
Mykhailenko Oleksandra <miksasha2003@gmail.com>
2022-06-24 17:19:06 +0300

    bug was fixed

--- app/src/main/java/com/example/myapplication/NewMedicineActivity.java
+++ app/src/main/java/com/example/myapplication/NewMedicineActivity.java
@@ -8,54 +8,27 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.navigation.NavigationView;
 
 public class NewMedicineActivity extends AppCompatActivity {
 
-    private DrawerLayout drawerLayout;
-    private ActionBarDrawerToggle actionBarDrawerToggle;
+    private Button add;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_medicine);
 
-        drawerLayout=(DrawerLayout) findViewById(R.id.dl);
-        actionBarDrawerToggle=new ActionBarDrawerToggle(this,drawerLayout,R.string.Open,R.string.Close);
-        actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        drawerLayout.addDrawerListener(actionBarDrawerToggle);
-        actionBarDrawerToggle.syncState();
+        add = findViewById(R.id.addButton);
 
-        NavigationView nav_view=(NavigationView) findViewById(R.id.nav_view);
-        nav_view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+        add.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                int id=item.getItemId();
-                if(id==R.id.nav_main){
-                    //Toast.makeText(MainActivity.this,"Main",Toast.LENGTH_SHORT).show();
-                    startActivity(new Intent(NewMedicineActivity.this, MainActivity.class));
-                }else if(id==R.id.nav_profile){
-                    //Toast.makeText(MainActivity.this,"Help",Toast.LENGTH_SHORT).show();
-                    startActivity(new Intent(NewMedicineActivity.this, ProfileActivity.class));
-                }else if(id==R.id.nav_water){
-                    //Toast.makeText(MainActivity.this,"Broadcast",Toast.LENGTH_SHORT).show();
-                    startActivity(new Intent(NewMedicineActivity.this, WaterBalanceActivity.class));
-                }else if(id==R.id.nav_feeling){
-                    //Toast.makeText(MainActivity.this,"Background",Toast.LENGTH_SHORT).show();
-                    startActivity(new Intent(NewMedicineActivity.this, FeelingActivity.class));
-                }else if(id==R.id.nav_help){
-                    //Toast.makeText(MainActivity.this,"Background",Toast.LENGTH_SHORT).show();
-                    startActivity(new Intent(NewMedicineActivity.this, InstructionActivity.class));
-                }
-                return true;
+            public void onClick(View view) {
+                // додаємо ліки у списор
             }
         });
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        return actionBarDrawerToggle.onOptionsItemSelected(item)||super.onOptionsItemSelected(item);
     }
 }
